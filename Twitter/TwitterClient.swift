481e1673cc99b689a227155fade03bcdb246c663
Scott Woody <swoody@dropbox.com>
2014-09-29 22:39:56 -0700

    Removed twitter creds

--- Twitter/TwitterClient.swift
+++ Twitter/TwitterClient.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 
-let twitterConsumerKey = "0jxBPb7rJkpIgiVvbyQWBqNND"
-let twitterConsumerSecret = "q96WME6vTZoFFpb7DAzBP9Msh6ZRacG4HtTzEnc8aFEAp1of6f"
+let twitterConsumerKey = ""
+let twitterConsumerSecret = ""
 let twitterBaseURL = NSURL(string: "https://api.twitter.com")
 
 
